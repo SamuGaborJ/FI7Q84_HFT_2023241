c679c100134603d0e152a5b850a68bb680f39c88
gabor <gabor@DESKTOP-0B60CNR>
2023-10-02 20:25:38 +0200

    Add project files.

---
+++ FI7Q84_HFT_2023241.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FI7Q84_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ FI7Q84_HFT_2023241.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FI7Q84_HFT_2023241.Logic
+{
+    public class Class1
+    {
+    }
+}
